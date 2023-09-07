9b6453661fc54892bbcc175bc1d2bc2c629d484b
ndn1665 <ndn1665@daum.net>
2023-09-07 17:16:34 +0900

    he

--- myproject/my_settings.py
+++ myproject/my_settings.py
@@ -3,8 +3,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'whating01',
         'USER': 'root',
-        'PASSWORD': 'scrrl0114',
-        'HOST': 'localhost', '124.60.120.44'
+        'PASSWORD': 'nohtaeil2@',
+        'HOST': "172.31.4.235",
         'PORT': '3306',
     }
 }
