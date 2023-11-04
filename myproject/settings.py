035ab92c56c10ddc94a291326cc1c34da586dc37
ndn1665 <ndn1665@daum.net>
2023-11-04 14:31:11 +0900

    template path change

--- myproject/settings.py
+++ myproject/settings.py
@@ -16,7 +16,7 @@ from myproject import my_settings
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent
 
 
 # Quick-start development settings - unsuitable for production
