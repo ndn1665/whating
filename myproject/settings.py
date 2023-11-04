30b3fea0ff2cabf042305f5518b94965e54f8154
ndn1665 <ndn1665@daum.net>
2023-11-04 13:27:01 +0900

    database to sqlite3

--- myproject/settings.py
+++ myproject/settings.py
@@ -82,7 +82,13 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = my_settings.DATABASES
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+# DATABASES = my_settings.DATABASES
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
