04deab230dfe74024b7b7ba94ec018d864d059f8
ndn1665 <ndn1665@daum.net>
2023-11-04 14:33:15 +0900

    template path change

--- myproject/settings.py
+++ myproject/settings.py
@@ -16,7 +16,7 @@ from myproject import my_settings
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'myproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'templates')],
+        'DIRS': [os.path.join(BASE_DIR,'myproject/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
